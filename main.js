48f7cb8affb2efb1d1244561999f774ad7772d1c
Francielle Dellamora Menegotto <francidellamora@gmail.com>
2021-09-10 08:23:42 -0300

    clean code

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import { initializeApp } from "firebase/app";
-const firebaseConfig = {
-  apiKey: "AIzaSyDAuoFLHf7uLpRn8o6m9NCeq8u_92NzYVk",
-  authDomain: "formcontact-49b54.firebaseapp.com",
-  projectId: "formcontact-49b54",
-  storageBucket: "formcontact-49b54.appspot.com",
-  messagingSenderId: "1021433148415",
-  appId: "1:1021433148415:web:39e9ab9d1651847dc46dca",
-};
+// import { initializeApp } from "firebase/app";
+// const firebaseConfig = {
+//   apiKey:
+//   authDomain:
+//   projectId: "
+//   storageBucket:
+//   messagingSenderId:
+//   appId:
+// };
 const app = initializeApp(firebaseConfig);
 
 let contactInfo = firebase.database().ref("infos");
@@ -38,4 +38,5 @@ function saveMessage(name, company, email, phone, message) {
     phone: phone,
     message: message,
   });
+  console.log(contactInfo);
 }
